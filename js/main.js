17b1b713b472ac37985d85a0e89ad1b8799becbc
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-06 11:59:57 +0100

    esercizio finito

--- js/main.js
+++ js/main.js
@@ -1,25 +1,17 @@
 //COLLEGHIAMO ELEMENTI A JS 
-const gridDom = document.getElementById('.griglia');
+const gridDom = document.getElementById('griglia');
 const selezione = document.getElementById('difficult');
 const play = document.getElementById ('btn');
 let numeroquadrati;
 let classBox;
-let gridgeneral;
-let fullgrid;
+
 
 play.addEventListener('click',
 function(){
-    
-   fullgrid = creazionegrigliacompleta();
-    
-    
-  
+   gridDom.innerHTML="";
+    creazionegrigliacompleta();
 }
 )
-
-
-
-
 
 function creazioneElemento(){
     const element = document.createElement('div');
@@ -34,18 +26,23 @@ function creazionegrigliacompleta(){
     } else if (selezione.value == "intermedio"){
         numeroquadrati= 81;
         classBox = "cellamedia";
+    } else if (selezione.value == "difficile"){
+        numeroquadrati=49;
+        classBox="celladifficile";
     }
-    gridgeneral = creazionegriglia(numeroquadrati,classBox);
+
+    creazionegriglia();
 }
 
-function creazionegriglia(numeroquadrati, classBox ){
+function creazionegriglia(){
     for(let i = 1; i <= numeroquadrati; i++){
         const element = creazioneElemento();
         element.classList.add(classBox);
         element.append(i);
         element.addEventListener('click', 
         function(){
-            this.classList.toogle('clicked');
+            console.log(this);
+            this.classList.toggle('clicked');
             console.log(`Hai scelto la casella numero ${i}`)
         }
         
